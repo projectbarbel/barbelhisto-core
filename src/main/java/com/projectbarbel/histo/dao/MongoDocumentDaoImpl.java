0e532d75f5b98a1891950f6ee609345f68251b0d
Niklas Schlimm <niklasschlimm@MacBook-Pro.fritz.box>
2019-01-14 09:28:23 +0100

    Initial commit with test cases

---
+++ src/main/java/com/projectbarbel/histo/dao/MongoDocumentDaoImpl.java
@@ -0,0 +1,32 @@
+package com.projectbarbel.histo.dao;
+
+import com.projectbarbel.histo.model.AbstractValueObject;
+
+public class MongoDocumentDaoImpl<T extends AbstractValueObject> implements DocumentDao<T> {
+
+	@Override
+	public T createDocument(T document) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public T updateDocument(T document) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void deleteDocument(String uniqueId) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void readDocument(String uniqueId) {
+		// TODO Auto-generated method stub
+		
+	}
+
+
+}
