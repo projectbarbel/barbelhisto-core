db143fb43d2995bfcf597493829cd6397e8e1ff2
Niklas Schlimm <ns103@hotmail.de>
2019-02-25 21:18:09 +0100

    New test case for BarbelModeProcessor

---
+++ src/test/java/org/projectbarbel/histo/BarbelModeProcessorTest.java
@@ -0,0 +1,38 @@
+package org.projectbarbel.histo;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+import org.projectbarbel.histo.model.BitemporalStamp;
+import org.projectbarbel.histo.model.BitemporalVersion;
+import org.projectbarbel.histo.model.DefaultDocument;
+import org.projectbarbel.histo.model.DefaultPojo;
+
+public class BarbelModeProcessorTest {
+
+    @Test
+    public void getDocumentIdFieldName() throws Exception {
+        assertEquals("documentId",BarbelMode.POJO.getDocumentIdFieldName(DefaultPojo.class));
+    }
+
+    @Test
+    public void testGetIdValue() throws Exception {
+        assertEquals("id",BarbelMode.POJO.getIdValue(new DefaultPojo("id", "data")).get());
+    }
+
+    @Test
+    public void getPersistenceObjectType_POJO() throws Exception {
+        assertEquals(BitemporalVersion.class,BarbelMode.POJO.getPersistenceObjectType(DefaultPojo.class));
+    }
+
+    @Test
+    public void getPersistenceObjectType_BITEMPORAL() throws Exception {
+        assertEquals(DefaultDocument.class,BarbelMode.BITEMPORAL.getPersistenceObjectType(DefaultDocument.class));
+    }
+    
+    @Test
+    public void getPersistenceObjectType() throws Exception {
+        assertEquals("bitemporalStamp",BarbelMode.BITEMPORAL.getStampFieldName(DefaultDocument.class, BitemporalStamp.class));
+    }
+    
+}
